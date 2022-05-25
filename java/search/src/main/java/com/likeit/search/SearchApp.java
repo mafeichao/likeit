2069a98b8fce222c5c289f16665a102179867f62
mafeichao <517426342@qq.com>
2022-05-25 20:12:20 +0800

    add search

--- java/search/src/main/java/com/likeit/search/SearchApp.java
+++ java/search/src/main/java/com/likeit/search/SearchApp.java
@@ -2,11 +2,12 @@ package com.likeit.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author mafeichao
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SearchApp {
     public static void main(String[] args) {
         SpringApplication.run(SearchApp.class, args);
