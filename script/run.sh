e173285abb399e65e4d52ca9115c67d189da7f7a
mafeichao <517426342@qq.com>
2022-06-05 23:36:48 +0800

    update java cp

--- script/run.sh
+++ script/run.sh
@@ -19,6 +19,6 @@ find ../log/*.log* -mtime +3 | xargs rm -rf
 today=$(python -c "$UTIL.today()")
 LOG="../log/likeit.log.$today"
 {
-    java -cp target/$JAR com.likeit.search.SearchApp > ../log/java.log.$today 2>&1 &
+    java -cp target/$JAR:conf/* com.likeit.search.SearchApp > ../log/java.log.$today 2>&1 &
     python web/app.py runserver -h 0.0.0.0 -p 8888 > ../log/python.log.$today 2>&1 &
 } >> $LOG 2>&1
